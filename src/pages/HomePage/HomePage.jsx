2f435dbf17b7b74a48e39157cecce047be877197
Denys Strashniuk <79088186+lordponchik@users.noreply.github.com>
2024-04-28 09:21:46 +0200

    add render movies day in component HomePage

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,4 +1,6 @@
 import { useEffect, useState } from 'react';
+import { useLocation } from 'react-router-dom';
+
 import { fetchTrendingsMovies } from 'services/api';
 import { Container } from 'components/Container/Container';
 import { MainHeading } from 'components/MainHeading/MainHeading';
@@ -6,13 +8,15 @@ import { Section } from 'components/Section/Section';
 import { MoviesList } from 'components/MoviesList/MoviesList';
 import { Loader } from 'components/Loader/Loader';
 
-export const HomePage = () => {
+const HomePage = () => {
   const [movies, setMovies] = useState(null);
+  const location = useLocation();
   const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
     const responseTrendingsMovie = async () => {
       setIsLoading(true);
+
       try {
         const { results } = await fetchTrendingsMovies();
 
@@ -32,9 +36,11 @@ export const HomePage = () => {
         <Container>
           <MainHeading title={'Trending Today'}></MainHeading>
           {isLoading && <Loader></Loader>}
-          {movies && <MoviesList movies={movies}></MoviesList>}
+          {movies && <MoviesList movies={movies} loc={location}></MoviesList>}
         </Container>
       </Section>
     </main>
   );
 };
+
+export default HomePage;
