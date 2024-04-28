dd138ee2040a37ae4d5bfd8eb7cde33795137d4c
Denys Strashniuk <79088186+lordponchik@users.noreply.github.com>
2024-04-28 09:27:33 +0200

    add Suspense in SharedLayout

--- src/components/SharedLayout/SharedLayout.jsx
+++ src/components/SharedLayout/SharedLayout.jsx
@@ -1,11 +1,19 @@
 import { Outlet } from 'react-router-dom';
-import { Header } from 'components/Header/Header';
+import { Suspense } from 'react';
 
-export const SharedLayout = () => {
+import { Header } from 'components/Header/Header';
+import { Loader } from 'components/Loader/Loader';
+
+const SharedLayout = () => {
   return (
     <>
       <Header></Header>
-      <Outlet></Outlet>
+
+      <Suspense fallback={<Loader />}>
+        <Outlet></Outlet>
+      </Suspense>
     </>
   );
 };
+
+export default SharedLayout;
