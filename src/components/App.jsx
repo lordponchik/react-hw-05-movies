4598f445da81e296e1c17b28a2c807f5c95760b7
Denys Strashniuk <79088186+lordponchik@users.noreply.github.com>
2024-04-28 09:29:34 +0200

    add lazy for import

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,34 @@
-import { HomePage } from 'pages/HomePage/HomePage';
-import { SharedLayout } from 'components/SharedLayout/SharedLayout';
 import { Route, Routes } from 'react-router-dom';
-import { MoviesPage } from 'pages/MoviesPage/MoviesPage';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { lazy } from 'react';
+
+import SharedLayout from 'components/SharedLayout/SharedLayout';
+
+const HomePage = lazy(() => import('pages/HomePage/HomePage'));
+const ErrorPage = lazy(() => import('pages/ErrorPage/ErrorPage'));
+const MoviesPage = lazy(() => import('pages/MoviesPage/MoviesPage'));
+const MovieDetailsPage = lazy(() =>
+  import('pages/MovieDetailsPage/MovieDetailsPage')
+);
+const Cast = lazy(() => import('components/Cast/Cast'));
+const Reviews = lazy(() => import('components/Reviews/Reviews'));
 
 export const App = () => {
   return (
-    <Routes>
-      <Route path="/" element={<SharedLayout />}>
-        <Route index element={<HomePage />}></Route>
-        <Route path="movies" element={<MoviesPage />}></Route>
-      </Route>
-    </Routes>
+    <>
+      <Routes>
+        <Route path="/" element={<SharedLayout />}>
+          <Route index element={<HomePage />}></Route>
+          <Route path="movies" element={<MoviesPage />}></Route>
+          <Route path="movies/:movieId" element={<MovieDetailsPage />}>
+            <Route path="cast" element={<Cast />}></Route>
+            <Route path="reviews" element={<Reviews />}></Route>
+          </Route>
+        </Route>
+        <Route path="*" element={<ErrorPage />}></Route>
+      </Routes>
+      <ToastContainer />
+    </>
   );
 };
